e4917a413bfd865159cf73d3c44526ff13ced72b
Alan Peabody <gapeabody@gmail.com>
2017-01-26 22:29:38 -0500

    Ignore nil times.

--- lib/newrelic_phoenix/ecto.ex
+++ lib/newrelic_phoenix/ecto.ex
@@ -20,14 +20,20 @@ defmodule NewRelicPhoenix.Ecto do
   Segment names will be command_on_table-(queue|query|decode) when we can tell.
   """
   def log(entry) do
-    record_segment({:db, segment(entry, "queue")},  to_ms(entry.queue_time))
-    record_segment({:db, segment(entry, "query")},  to_ms(entry.query_time))
-    record_segment({:db, segment(entry, "decode")}, to_ms(entry.decode_time))
+    if entry.queue_time do
+      record_segment({:db, segment(entry, "queue")},  to_ms(entry.queue_time))
+    end
+    if entry.query_time do
+      record_segment({:db, segment(entry, "query")},  to_ms(entry.query_time))
+    end
+    if entry.decode_time do
+      record_segment({:db, segment(entry, "decode")}, to_ms(entry.decode_time))
+    end
   end
 
-  defp segment(%{source: source, result: {:ok, %{command: command}}}, part) when is_binary(source),
+  defp segment(%{source: source, result: {:ok, %{command: command}}}, part) when is_binary(source) and is_binary(command),
     do: "Ecto.#{part}.#{command}-#{source}"
-  defp segment(%{source: _, result: {:ok, %{command: command}}}, part),
+  defp segment(%{source: _, result: {:ok, %{command: command}}}, part) when is_binary(command),
     do: "Ecto.#{part}.#{command}"
   defp segment(_, part),
     do: "Ecto.#{part}.unknown"
